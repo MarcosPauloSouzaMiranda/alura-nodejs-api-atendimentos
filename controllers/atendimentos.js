1dcfc2be3b54fe510399d89ec58695929ef0bf74
MARCOS PAULO SOUZA MIRANDA <marcosmiranda@embare.com.br>
2019-10-30 13:52:38 -0300

    Inclusão do body parser de dados e da rota POST para criação de atendimentos!

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -4,4 +4,14 @@ module.exports = app => {
         res.send('<h1>Servidor Online!</h1>');
     });
 
+    app.post('/atendimentos', (req, res) => {
+
+        var data = req.body;
+
+        console.log(data);
+
+        res.send('Requisição realizada por método POST!');
+        
+    });
+
 }
